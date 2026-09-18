ce42b954e06ceb58ccf48301686de51ba596cf20
curseflux <cursedflux@gmail.com>
2026-09-18 01:49:01 -0500

    bird solution

--- pulseflight/answer.py
+++ pulseflight/answer.py
@@ -8,7 +8,12 @@ import math
 
 DEBUG = {}
 
-THRESH = 100
+# THRESH = 0
+# THRESH = 50
+# THRESH = 80
+THRESH = 100 #
+# THRESH = 110
+# THRESH = 130
 
 def advance(state, action, telemetry):
 
@@ -63,8 +68,6 @@ def check_crashed(state, telemetry):
 
         gate_r = gate['x'] + gate['half_width']
         gate_l = gate['x'] - gate['half_width']
-
-        # dist = math.sqrt((bird_c[0])**2 + (bird_c[1])**2)
 
         if bird_t <= gate_hi or bird_b >= gate_lo:
             if bird_r >= gate_l and gate_r >= bird_l:
@@ -141,8 +144,8 @@ def decide(telemetry):
     }
 
     # n = 40
-    # n = 30
-    n = 20
+    n = 30
+    # n = 20
 
     best_act = ("glide", 0)
 
@@ -166,8 +169,10 @@ def decide(telemetry):
             best_act = (action, score)
 
     DEBUG.update(act=best_act[0], score=best_act[1])
-        
+    
     return best_act[0]
+
+    # Passed 15/15 | average score 100.0/100 | slowest decision 39.42 ms
 
 
     # {'clock': {'tick': 0, 'limit': 363, 'dt': 0.04}, 'bird': {'x': 0.0, 'y': 215.5950005641389, 'vy': 0.0, 'cooldown': 0}, 'course': {'height': 397.6439135264536, 'cleared': 0, 'total': 6}, 'physics': {'dt': 0.04, 'gravity': 336.2915069968503, 'drag': 0.19066378452737584, 'impulse': 72.05971969439541, 'recharge': 3, 'rise_cap': 206.02957722228192, 'fall_cap': 189.72187040480443, 'speed': 142.17868454852166, 'radius': 9.965879856142958}, 'gates': [{'id': 0, 'x': 324.1674007706294, 'half_width': 12.18342762643034, 'centre': 167.02191845435527, 'gap': 79.58317772597394, 'amplitude': 0, 'period': 121.51063598838361, 'phase': 1.7646457961899387, 'top': 127.2303295913683, 'bottom': 206.81350731734224}, {'id': 1, 'x': 580.0890329579684, 'half_width': 19.411890378528064, 'centre': 236.04661027449822, 'gap': 89.99244311246707, 'amplitude': 0, 'period': 116.7313694187013, 'phase': -1.2668491566638753, 'top': 191.05038871826468, 'bottom': 281.04283183073176}, {'id': 2, 'x': 813.2620756175439, 'half_width': 18.158980293532906, 'centre': 272.25955921333093, 'gap': 74.2587230689002, 'amplitude': 0, 'period': 123.02363290869611, 'phase': 2.9645928503124015, 'top': 235.13019767888085, 'bottom': 309.388920747781}]}
